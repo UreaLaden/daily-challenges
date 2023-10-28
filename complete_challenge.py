778eb798e8303712299d4104a94c77b49c3d2c12
Leaundrae McKinney <lmckinney@go-planet.com>
2023-10-27 22:23:48 -0400

    Included command line arguments for completing challenges

--- complete_challenge.py
+++ complete_challenge.py
@@ -1,11 +1,22 @@
 import json 
+import argparse
 
 def complete_challenge(name:str):
-    with open('challenges.json','rw') as file:
+    with open('challenges.json','r') as file:
         data = json.loads(file.read())
     
     if name in data:
-        data[name]['is_completed'] = True
+        data[name][0]['is_completed'] = True
+    else:
+        print("Invalid Name")
+        return
     
     with open('challenges.json','w') as file:
         json.dump(data,file,indent=4)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Command line argument for code challenge completion")
+    parser.add_argument("arg1",type=str, help="Challenge name found in 'challenges.json'")
+    args = parser.parse_args()
+    challenge_name = args.arg1
+    complete_challenge(challenge_name)
